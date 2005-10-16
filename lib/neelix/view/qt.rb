8f5d29ba2b7e59263a514cb9a4bd9e92ef60f09d
hans <hans@fugal.net>
2005-10-16 12:55:16 -0700

    populate shelf
    It's a modest beginning, but it's a beginning.
    
    darcs-hash:20051016195516-28bda-745073a5bcd77849fb7fccc6b34f89770f0f3231.gz

--- lib/neelix/view/qt.rb
+++ lib/neelix/view/qt.rb
@@ -1,2 +1,30 @@
 require 'neelix/view/qt/mw.rb'
 require 'neelix/view/qt/aboutdialog.rb'
+
+class RecipeItem < Qt::ListViewItem
+  def initialize(parent,recipe)
+    @recipe = recipe
+    super(parent)
+  end
+
+  def text(col=0)
+    case col
+    when 0
+      @recipe.name
+    else
+      nil
+    end
+  end
+end
+
+class NeelixMainWindow < NeelixMainWindowBase
+  def initialize(*k)
+    super(*k)
+
+    @shelf.clear
+    recipes = Recipe.find(:all)
+    recipes.each {|r| RecipeItem.new(@shelf,r)}
+  end
+
+
+end
